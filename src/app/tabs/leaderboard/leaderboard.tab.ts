45a06603d27be147448641d338bd1676055b9cdd
Chris Sullivan <csullivan@reliaslearning.com>
2019-05-09 13:11:07 -0400

    added leaderboard stuff

--- src/app/tabs/leaderboard/leaderboard.tab.ts
+++ src/app/tabs/leaderboard/leaderboard.tab.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { TeamModel } from '../team/team.model';
-import { switchMap } from 'rxjs/operators';
+import { Observable, forkJoin } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+import { LeaderboardService } from 'src/app/shared/leaderboard.service';
+import { TeamService } from 'src/app/shared/team.service';
+import { TeamRecordModel } from 'src/app/shared/models/team-record.model';
+import { TeamModel } from 'src/app/shared/models/team.model';
 
 @Component({
   selector: 'app-leaderboard-tab',
@@ -9,24 +12,31 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['leaderboard.tab.scss']
 })
 export class LeaderboardTab implements OnInit {
-  teams: Observable<TeamModel[]>;
-  myBestTeam: TeamModel;
-  myRank: number;
-  constructor() { }
+teamRankings: {team: TeamModel, ranking: TeamRecordModel}[];
+myBestTeam: TeamModel;
+myRank: number;
+  constructor(private leaderboardService: LeaderboardService, private teamsService: TeamService) {}
 
   ngOnInit(): void {
-    // this.teams = this.db.getTeams();
-    // this.db.getMyTeam(1).pipe( switchMap( x => {
-    //   if (x.length > 0) {
-    //     this.myBestTeam = x[0];
-    //   }
-    //   return this.db.getTeams();
-    // })).subscribe(x => {
-    //   this.myRank = x.findIndex(x => x.name === this.myBestTeam.name) + 1;
-    // });
+    forkJoin([this.teamsService.getTeams(), this.leaderboardService.getTeamRecordByRuleset(1)]).pipe( map(([teams, rankings]) => {
+      return teams.map(team => {
+        const ranking = rankings.find(x => x.teamId === team.id);
+        return{team, ranking};
+      }).sort((a, b) => {
+        return a.ranking.elo >= b.ranking.elo ? 1 : 0;
+      });
+    })).subscribe(x => this.teamRankings = x);
   }
 
   expand(team: any) {
+    team['expanded'] = !team['expanded'];
+  }
 
+  getMyBestTeam(userId: number): {team:{team: TeamModel, ranking: TeamRecordModel}, rank: number} {
+      const teamRank = this.teamRankings.find(x => x.team.users.find(u => u.id === userId) !== undefined);
+      return {
+      team: teamRank,
+      rank: this.teamRankings.indexOf(teamRank) + 1
+    };
   }
 }
