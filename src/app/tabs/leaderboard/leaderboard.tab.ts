59873d6c11c21446f7966442bc9601eaac45ffbf
Chris Sullivan <csullivan@reliaslearning.com>
2019-05-09 17:52:20 -0400

    fixed stuff with leaderboards

--- src/app/tabs/leaderboard/leaderboard.tab.ts
+++ src/app/tabs/leaderboard/leaderboard.tab.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Observable, forkJoin } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
 import { LeaderboardService } from 'src/app/shared/leaderboard.service';
 import { TeamService } from 'src/app/shared/team.service';
 import { TeamRecordModel } from 'src/app/shared/models/team-record.model';
 import { TeamModel } from 'src/app/shared/models/team.model';
+import { RulesetService } from 'src/app/shared/ruleset.service';
+import { RulesetModel } from 'src/app/shared/models/ruleset.model';
 
 @Component({
   selector: 'app-leaderboard-tab',
@@ -16,10 +18,40 @@ export class LeaderboardTab implements OnInit {
   myBestTeam: {team: {team: TeamModel, ranking: TeamRecordModel}, rank: number};
   myRank: number;
   userId = Number.parseInt(localStorage.getItem('userId'));
-  constructor(private leaderboardService: LeaderboardService, private teamsService: TeamService) {}
+  rulesets: RulesetModel[];
+  ruleset: number;
+  constructor(
+    private leaderboardService: LeaderboardService,
+    private teamsService: TeamService,
+    private rulesetService: RulesetService,
+    private det: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-    forkJoin([this.teamsService.getTeams(), this.leaderboardService.getTeamRecordByRuleset(1)]).pipe( map(([teams, rankings]) => {
+    this.ruleset = 1;
+    this.rulesetService.getRulesets().subscribe(x => {
+      this.rulesets = x;
+      this.det.detectChanges();
+    });
+    this.getLeaderboards(1);
+  }
+
+  checkMyTeams(i: number) {
+    return (this.teamRankings[i].team.users.find(x => x.id === this.userId) === undefined);
+  }
+
+  expand(team: any) {
+    team['expanded'] = !team['expanded'];
+  }
+
+  changeRuleset(val: any) {
+    this.getLeaderboards(val.detail.value);
+  }
+
+  getLeaderboards(val: number) {
+    forkJoin([
+      this.teamsService.getTeams(),
+      this.leaderboardService.getTeamRecordByRuleset(val)
+    ]).pipe( map(([teams, rankings]) => {
       if (teams.length > 0) {
         return rankings.map(ranking => {
           const team = teams.find(x => x.id === ranking.teamId);
@@ -36,13 +68,5 @@ export class LeaderboardTab implements OnInit {
       rank: this.teamRankings.indexOf(teamRank) + 1
       };
     });
-  }
-
-  checkMyTeams(i: number) {
-    return (this.teamRankings[i].team.users.find(x => x.id === this.userId) === undefined);
-  }
-
-  expand(team: any) {
-    team['expanded'] = !team['expanded'];
   }
 }
