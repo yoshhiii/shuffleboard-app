6bb067aae8674e4857eebf4a2ae99bedc5e10e35
Ashley Edds <aedds@relias.com>
2019-05-09 20:53:26 -0400

    tabs css

--- src/app/shared/icons/leaderboard/leaderboard.component.ts
+++ src/app/shared/icons/leaderboard/leaderboard.component.ts
@@ -7,4 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class LeaderboardIconComponent {
   @Input() svgSize;
+  @Input() svgColor;
 }
--- src/app/shared/icons/scoreboard/scoreboard.component.ts
+++ src/app/shared/icons/scoreboard/scoreboard.component.ts
@@ -7,4 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class ScoreboardIconComponent {
   @Input() svgSize;
+  @Input() svgColor;
 }
